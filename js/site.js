6f1a5b7db1ece5734be3b4288b811a2ad6a4ce76
krishns18 <krishna.shailesh@gmail.com>
2017-09-17 18:18:13 -0400

    Updated few cosmetic glitches

--- js/site.js
+++ js/site.js
@@ -1,6 +1,23 @@
-(function () {
-
-    $('.progress-content').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+jQuery(function ($) { 
+    'use strict';
+    
+    $('.count-wrap').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).unbind('inview');
+        }
+    });
+    
+    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
             $.each($('div.progress-bar'),function(){
                 $(this).css('width', $(this).attr('aria-valuenow')+'%');
@@ -8,18 +25,83 @@
             $(this).unbind('inview');
         }
     });
-    $('.rating-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+    
+    $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
-            $.each($('div.progress-bar'),function(){
-                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            $('.my-pie').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor:'#f4bf00',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 90,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+            $('.my-pie-2').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor:'#f7941d',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 90,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+            $('.my-pie-3').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor:'#f26522',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 90,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+            $('.my-pie-4').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor:'#ed1c24',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 90,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
             });
             $(this).unbind('inview');
         }
     });
+    
+    (function () {
 
-}());
+        new WOW({
 
+            mobile:  false
 
+        }).init();
 
+    }());
+
+});
 
 
